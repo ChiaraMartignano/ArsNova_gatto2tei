127380fa392051148ce2b151cf30f246dce57bb8
Chiara Martignano <chiaramartignano@gmail.com>
2021-03-11 17:55:56 +0100

    Fix bugs

--- converter.js
+++ converter.js
@@ -31,33 +31,36 @@ var addTextElem = function(TEINode) {
 }
 
 var replaceLElems = function (TEINode) {
-  var lg = TEINode.getElementsByTagName('lg')[0];
-  var newLg = lg.cloneNode(false);
-  var LText = [];
-  var i = 0;
-  while (i < lg.childNodes.length) {
-    if (lg.childNodes[i].tagName === 'lb') {
-      newLg.removeChild(lg.childNodes[i]);
-    } else if (lg.childNodes[i].tagName === 'l') {
+  var lgs = TEINode.getElementsByTagName('lg');
+  for (var z = 0; z < lgs.length; z++) {
+    var lg = lgs[z];
+    var newLg = lg.cloneNode(false);
+    var LText = [];
+    var i = 0;
+    while (i < lg.childNodes.length) {
+      if (lg.childNodes[i].tagName === 'lb') {
+        newLg.removeChild(lg.childNodes[i]);
+      } else if (lg.childNodes[i].tagName === 'l') {
+        var newL = TEINode.ownerDocument.createElement('l');
+        for (var k = 0; k < LText.length; k++) {
+          newL.appendChild(LText[k]);
+        }
+        newLg.appendChild(newL);
+        LText = [];
+      } else {
+        LText.push(lg.childNodes[i].cloneNode(true));
+      }
+      i++;
+    }
+    if (LText.length > 0) {
       var newL = TEINode.ownerDocument.createElement('l');
       for (var k = 0; k < LText.length; k++) {
         newL.appendChild(LText[k]);
       }
       newLg.appendChild(newL);
-      LText = [];
-    } else {
-      LText.push(lg.childNodes[i].cloneNode(true));
     }
-    i++;
+    lg.parentNode.replaceChild(newLg, lg);
   }
-  if (LText.length > 0) {
-    var newL = TEINode.ownerDocument.createElement('l');
-    for (var k = 0; k < LText.length; k++) {
-      newL.appendChild(LText[k]);
-    }
-    newLg.appendChild(newL);
-  }
-  lg.parentNode.replaceChild(newLg, lg);
 }
 
 var convertToTEI = function(node) {
@@ -74,13 +77,13 @@ var convertToTEI = function(node) {
 }
 
 var convertFile = function(file) {
-  fs.readFile('./input/' + file, 'utf8', function(err, data) {
-    if (err) { throw err }
+  fs.readFile('./input/' + file.name, 'utf8', function(err, data) {
+    if (err) { console.log(err); throw err }
     if (data) {
       var dom = parser.parseFromString(data, 'text/xml');
       var text = dom.getElementsByTagName('TEI')[0];
       var tei = convertToTEI(text);
-      var fileName = file.replace('xmlgat', 'tei');
+      var fileName = file.name.replace('xmlgat', 'tei');
       fs.writeFile('./output/' + fileName, SCHEMA + tei, function(err) {
         if (err) { throw err }
       })
@@ -89,7 +92,7 @@ var convertFile = function(file) {
 }
 
 fs.readdir('./input', {encoding: 'utf8', withFileTypes: true}, function(err, files) {
-  if (err) { throw err }
+  if (err) { console.log(err); throw err }
   files.map(function(file) {
     convertFile(file);
   })
